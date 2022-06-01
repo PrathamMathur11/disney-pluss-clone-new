89d0377414d518f1d13be2551d3af1772ed66a00
PrathamMathur11 <mathurpratham010205@gmail.com>
2022-06-01 19:44:07 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  // Put firebaseConfig of your Database(firebase)
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
